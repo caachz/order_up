916b92564707d97cf192738e504d89c4588e967d
Cassie Achzenick <caachzenick@gmail.com>
2020-02-21 09:34:32 -0700

    Add user story 2 test

--- spec/features/dish/dish_show_spec.rb
+++ spec/features/dish/dish_show_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "When a visitor goes to the dish show page" do
   it "shows the name of the chef and ingredients of the dish" do
     chef = Chef.create!(name: "Ben Little")
 
-
     dish = Dish.create!(name: "Spicy Thai", description: "very very spicy", chef: chef)
 
     ingredient1 = Ingredient.create!(name: "Chili", calories: 4)
@@ -21,5 +20,23 @@ RSpec.describe "When a visitor goes to the dish show page" do
     expect(page).to have_content("Chili")
     expect(page).to have_content("noodles")
     expect(page).to have_content("sauce")
+  end
+
+  it "sees total calorie count per dish" do
+    chef = Chef.create!(name: "Ben Little")
+
+    dish = Dish.create!(name: "Spicy Thai", description: "very very spicy", chef: chef)
+
+    ingredient1 = Ingredient.create!(name: "Chili", calories: 4)
+    ingredient2 = Ingredient.create!(name: "noodles", calories: 12)
+    ingredient3 = Ingredient.create!(name: "sauce", calories: 2)
+
+    dish.ingredients << ingredient1
+    dish.ingredients << ingredient2
+    dish.ingredients << ingredient3
+
+    visit "/dishes/#{dish.id}"
+
+    expect(page).to have_content("Total Calolires: 18")
   end
 end
