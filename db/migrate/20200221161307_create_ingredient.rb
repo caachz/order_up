0dce1286736ed7f1350bcbbee5834221e55fd88f
Cassie Achzenick <caachzenick@gmail.com>
2020-02-21 09:32:25 -0700

    User story 1 complete

---
+++ db/migrate/20200221161307_create_ingredient.rb
@@ -0,0 +1,12 @@
+class CreateIngredient < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ingredients do |t|
+        t.string :name
+        t.integer :calories
+    end
+    create_table :dish_ingredients do |t|
+      t.references :ingredient, foreign_key: true
+      t.references :dish, foreign_key: true
+    end
+  end
+end
---
+++ spec/features/dish/dish_show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "When a visitor goes to the dish show page" do
+  it "shows the name of the chef and ingredients of the dish" do
+    chef = Chef.create!(name: "Ben Little")
+
+
+    dish = Dish.create!(name: "Spicy Thai", description: "very very spicy", chef: chef)
+
+    ingredient1 = Ingredient.create!(name: "Chili", calories: 4)
+    ingredient2 = Ingredient.create!(name: "noodles", calories: 12)
+    ingredient3 = Ingredient.create!(name: "sauce", calories: 2)
+
+    dish.ingredients << ingredient1
+    dish.ingredients << ingredient2
+    dish.ingredients << ingredient3
+
+    visit "/dishes/#{dish.id}"
+
+    expect(page).to have_content("Ben Little")
+    expect(page).to have_content("Chili")
+    expect(page).to have_content("noodles")
+    expect(page).to have_content("sauce")
+  end
+end
